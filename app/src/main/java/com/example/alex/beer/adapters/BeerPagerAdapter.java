8a0f95a8bb90f200bdb3bb9d0f48f0568c28dd71
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-12 10:58:01 +0300

    added BeerPagerAdapter.java

--- app/src/main/java/com/example/alex/beer/adapters/BeerPagerAdapter.java
+++ app/src/main/java/com/example/alex/beer/adapters/BeerPagerAdapter.java
@@ -1,8 +1,39 @@
 package com.example.alex.beer.adapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.alex.beer.models.Beer;
+import com.example.alex.beer.ui.BeerDetailFragment;
+
+import java.util.ArrayList;
+
 /**
  * Created by alex on 6/12/17.
  */
 
-public class BeerPagerAdapter {
+public class BeerPagerAdapter  extends FragmentPagerAdapter {
+    private ArrayList<Beer> mBeers;
+
+    public BeerPagerAdapter(FragmentManager fm, ArrayList<Beer> beers) {
+        super(fm);
+        mBeers = beers;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BeerDetailFragment.newInstance(mBeers.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mBeers.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mBeers.get(position).getName();
+    }
 }
+
